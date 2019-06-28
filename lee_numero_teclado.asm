a3d8593400c64f98eeb1a19dcb4f773e2b00ba9c
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-06-28 16:14:27 -0400

    Programa que lee 5 numeros Ingresados por teclado y luego los Imprime en pantalla

---
+++ lee_numero_teclado.asm
@@ -0,0 +1,45 @@
+section .data								;Segmento de Dato
+	userMsg db 'Please entere a Numbre: '	;Pide al Usuario que Ingrese un Número
+	lenUserMsg equ $-userMsg				;La Longitud del Mensaje
+	dispMsg db 'You Have Entered: '
+	lenDispMsg equ $-dispMsg
+
+section .bss		;Datos sin inicializar
+	num resb 5
+
+section .text
+	global _start
+
+_start:
+	mov eax,4
+	mov ebx,1
+	mov	ecx,userMsg
+	mov	eax,lenUserMsg
+	int 80h
+
+	;Lee y almacena la entrada del usuario
+	mov eax,3
+	mov ebx,2
+	mov ecx,num
+	mov	edx,5
+	int 80h
+
+	;Mensaje de Salida 'The entered number is'
+	mov eax,4
+	mov ebx,1
+	mov ecx,dispMsg
+	mov edx,lenDispMsg
+	int 80h
+
+	;Salida del número Ingresado
+	mov eax,4
+	mov ebx,1
+	mov	ecx,num
+	mov edx,5
+	int 80h
+
+	;Código de Salida
+	mov eax,1
+	mov ebx,0
+	int 80h
+
