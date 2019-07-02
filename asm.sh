6bbc90550ae4abdbbb81fb40fc185b9744b2d5e5
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-02 11:26:27 -0400

    Añadiendo comentarios al Scrpt

--- asm.sh
+++ asm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #Scrpt que Compila codigo Ensamblador
-nasm -f elf $1.asm   
+echo "Generando Archivo "$1".o";
+nasm -f elf $1.asm;
 
 #$1 Representa el parametro que le pasamos al Script
-
-ld -m elf_i386 -s -o $1 $1.o
+echo "Enlazando y Creando Ejecutable  ";
+ld -m elf_i386 -s -o $1 $1.o;
 
 ./$1
